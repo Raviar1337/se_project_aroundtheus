91cf96854dab842b65ddc50f6a7b31b8a0bf3443
Raviar <raviarindustries@gmail.com>
2023-06-26 07:55:53 -0400

    feat: Edit profile functionality

--- scripts/index.js
+++ scripts/index.js
@@ -25,22 +25,58 @@ let initialCards = [
   },
 ];
 
+//----------------- LET DECLARATIONS ------------------------------------------------
+
 let modal = document.querySelector(".modal");
 
 let modalCloseButton = document.querySelector(".modal__close-button");
 
 let editButton = document.querySelector(".edit-button");
 
+let profileName = document.querySelector(".profile__title");
+
+let profileDescription = document.querySelector(".profile__description");
+
+let inputProfileName = document.querySelector(".modal__input_type_name");
+
+let inputProfileDescription = document.querySelector(
+  ".modal__input_type_description"
+);
+
+let modalSaveButton = document.querySelector(".modal__save-button");
+
+let modalForm = document.querySelector(".modal__form");
+//_________________________________________________________________________________________
+
+//----------------------FUNCTIONS --------------------------------------------------------
+
 function openModal() {
   modal.classList.add("modal_opened");
+  inputProfileName.value = profileName.textContent;
+  inputProfileDescription.value = profileDescription.textContent;
 }
 
 function closeModal() {
   modal.classList.remove("modal_opened");
 }
 
+function saveEditProfileChanges(evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal_opened");
+  profileName.textContent = inputProfileName.value;
+  profileDescription.textContent = inputProfileDescription.value;
+}
+
+//________________________________________________________________________________________________
+
+//------------------------ EVENTS ---------------------------------------------------------------
+
 editButton.addEventListener("click", openModal);
 
 modalCloseButton.addEventListener("click", closeModal);
+
+modalForm.addEventListener("submit", saveEditProfileChanges);
+
+//________________________________________________________________________________________________
 
 console.log(modalCloseButton);
