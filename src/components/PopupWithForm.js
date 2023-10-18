38c8561a4b66ff938ff9c42a57259caa821b9627
Raviar <raviarindustries@gmail.com>
2023-10-18 19:18:40 -0400

    make minor changes to css and evt prevent default testing

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -19,7 +19,7 @@ export default class PopupWithForm extends Popup {
   }
 
   submit = (evt) => {
-    //evt.preventDefault();
+    evt.preventDefault();
     this._handleFormSubmit(this._getInputValues());
 
     this.close();
