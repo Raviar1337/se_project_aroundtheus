1ffa34dbf74bfba92e7f2effe764c26f625ef10f
Raviar <raviarindustries@gmail.com>
2023-10-23 19:12:22 -0400

    some minor changes removing evt prevent default and then a larger push

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,6 +10,7 @@ export default class Card {
   constructor(data, templateSelector, handleImageClick, handleDeleteClick) {
     this._name = data.name;
     this._link = data.link;
+    this._id = data._id;
     this._templateSelector = templateSelector;
     this._handleImageClick = handleImageClick;
     this._handleDeleteClick = handleDeleteClick;
